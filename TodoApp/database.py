180eb34abced16e821fb9a611088b13031d489ca
erfan <erfansad.83@gamil.com>
2024-08-17 16:07:53 +0330

    connect application to mysql

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:erf138383@127.0.0.1:3306/TodoApplicationDatabase'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
