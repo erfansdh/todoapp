1c555575c64ea7c11d02254e179235ef4964df90
erfan <erfansad.83@gamil.com>
2024-08-28 21:07:34 +0330

    add requirements file

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -1,63 +1,69 @@
-from typing import Annotated
-from pydantic import BaseModel, Field
+import sys
+sys.path.append("..")
+
+from starlette import status
+from starlette.responses import RedirectResponse
+from fastapi import Depends, APIRouter, Request, Form
+import models
+from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException, status, Path
-from models import Todos, Users
-from database import SessionLocal
-from .auth import get_current_user
-from passlib.context import CryptContext
+from pydantic import BaseModel
+from .auth import get_current_user, verify_password, get_password_hash
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 router = APIRouter(
-    prefix='/user',
-    tags=['user']
+    prefix="/users",
+    tags=["users"],
+    responses={404: {"description": "Not Found"}}
 )
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
 
 
 def get_db():
-    db = SessionLocal()
     try:
+        db = SessionLocal()
         yield db
     finally:
         db.close()
 
-db_dependency = Annotated[Session, Depends(get_db)]
-user_dependency = Annotated[dict, Depends(get_current_user)]
-bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
 
 class UserVerification(BaseModel):
+    username: str
     password: str
-    new_password: str = Field(min_length=6)
+    new_password: str
 
-
-@router.get("/", status_code=status.HTTP_200_OK)
-async def get_user(user: user_dependency, db:db_dependency):
+@router.get("/edit-password", response_class=HTMLResponse)
+async def edit_user_view(request: Request):
+    user = await get_current_user(request)
     if user is None:
-        raise HTTPException(status_code=401, detail='Authentication Failed')
-    return db.query(Users).filter(Users.id == user.get('id')).first()
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    return templates.TemplateResponse("edit-user-password.html", {"request": request, "user": user})
 
-@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
-async def change_password(user: user_dependency, db: db_dependency, user_verification: UserVerification):
+@router.post("/edit-password", response_class=HTMLResponse)
+async def user_password_change(request: Request, username: str = Form(...),
+                               password: str = Form(...), password2: str = Form(...),
+                               db :Session =Depends(get_db)):
+    user = await get_current_user(request)
     if user is None:
-        raise HTTPException(status_code=401, detail='Authentication Failed')
-    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
 
-    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
-        raise HTTPException(status_code=401, detail='Error on password change')
-    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
-    db.add(user_model)
-    db.commit()
+    user_data = db.query(models.Users).filter(models.Users.username == username).first()
+    msg = "Invalid username or password"
 
-@router.put("/phonenumber/{phone_number}",status_code=status.HTTP_204_NO_CONTENT)
-async def chenage_phone_number(user: user_dependency, db: db_dependency, phone_number: str):
-    if user is None:
-        raise HTTPException(status_code=401, detail='Authentication Failed')
-    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
-    user_model.phone_number = phone_number
-    db.add(user_model)
-    db.commit()
+    if user_data is not None:
+        if username == user_data.username and verify_password(password, user_data.hashed_password):
+            user_data.hashed_password = get_password_hash(password2)
+            db.add(user_data)
+            db.commit()
+            msg = "Password updated"
 
+    return templates.TemplateResponse("edit-user-password.html", {"request": request, "user": user, "msg": msg})
 
 
 
